14270d4e98068442a46cf5d23d819e434925a612
Anhtt9x <anhtt454598@gmail.com>
2024-08-24 23:28:46 +0700

    update

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 from llama_index.core import SimpleDirectoryReader,VectorStoreIndex
 from llama_index.llms.gemini import Gemini
 from IPython.display import Markdown
-from llama_index.core import ServiceContext, StorageContext, load_index_from_storage
+from llama_index.core import Settings, StorageContext, load_index_from_storage,ServiceContext
 import google.generativeai as genai
 from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.core.node_parser import SentenceSplitter
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-google_api_key = os.getenv("GOOGLE_API_KEY")
 
-genai.configure(api_key=google_api_key)
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
 
 data = SimpleDirectoryReader("data")
 
 documents = data.load_data()
 
-model = genai.GenerativeModel("gemini-1.5-pro")
+gemini=Gemini(model_name="models/gemini-1.5-pro",api_key=google_api_key)
 
-embedding = GeminiEmbedding(model_name="models/text-embedding-004")
+Settings.llm = gemini
 
-service_context = ServiceContext.from_defaults(llm=model, embed_model=embedding,chunk_size=800,chunk_overlap=200)
+Settings.embed_model = GeminiEmbedding(model_name="models/text-embedding-004",api_key=google_api_key)
 
-index = VectorStoreIndex.from_documents(documents=documents,service_context=service_context)
+Settings.node_parser = SentenceSplitter(chunk_size=500,chunk_overlap=20)
+
+
+index = VectorStoreIndex.from_documents(documents=documents)
+
+index.storage_context.persist("storage")
+
+engine = index.as_query_engine()
+
+response = engine.query("Your query here")
+print(response.response)
 
